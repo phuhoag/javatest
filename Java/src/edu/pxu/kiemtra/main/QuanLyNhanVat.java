ff79de92cffdd32adba6d870335db5b0383563c9
phuhoag <147980728+phuhoag@users.noreply.github.com>
2024-06-21 21:58:40 +0700

    QuanLyNhanVat.java

--- Java/src/edu/pxu/kiemtra/main/QuanLyNhanVat.java
+++ Java/src/edu/pxu/kiemtra/main/QuanLyNhanVat.java
@@ -15,9 +15,18 @@ public class QuanLyNhanVat {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
 		List<NhanVat> dsnv1 = new ArrayList<NhanVat>();
-		System.out.println("hellword");
+		KyBinh nv1 = new KyBinh("Phu", 70, new CungTen(70, 300), true, true);
+        	dsnv1.add(nv1);
+        	KyBinh nv2 = new KyBinh("huy", 55, new CungTen(20, 200), false, true);
+        	dsnv1.add(nv2);
+        	KyBinh nv3 = new KyBinh("do", 92, new CungTen(100, 500), true, true);
+        	dsnv1.add(nv3);
+       	 	PhuThuy nv4 = new PhuThuy("Tuan", 63, new Choi(50, 40), true);
+        	dsnv1.add(nv4);
+        	BinhDoan bd1 = new BinhDoan(1509, "Binh doan tien cong", dsnv1);
+        	System.out.println("Danh sach binh doan " + bd1.getTenBD());
+        	System.out.println(bd1);
 		
 		
 		
